a68132022db655be27b87f1791d752eb7bd91a2c
ddangulea <77763777+ddangulea@users.noreply.github.com>
2024-01-18 15:47:02 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 
 import streamlit
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -9,7 +10,7 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+# import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -73,3 +74,5 @@ streamlit.write('The user entered ', fruit_choice)
 
 
 my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+
+
